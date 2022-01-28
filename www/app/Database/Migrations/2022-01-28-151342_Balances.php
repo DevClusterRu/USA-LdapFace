6f469fc636dba79204456613807d4fbcc96e8ac2
Nastya <webkrilya@gmail.com>
2022-01-29 00:01:16 +0300

    zoom

---
+++ www/app/Database/Migrations/2022-01-28-151342_Balances.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Balances extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+            ],
+            'amount' => [
+                'type' => 'INT',
+                'unsigned' => true,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+            ],
+            'deleted_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        // $this->forge->addForeignKey('role_id','user_roles','id');
+        $this->forge->createTable('balances');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('balances');
+    }
+}
